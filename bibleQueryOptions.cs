176bef1b0cf26dfffdbbddf919c61c9b0cb4fe46
APIVerve Team <hello@apiverve.com>
2024-05-14 15:21:05 -0500

    Bug fixes and Improvements

---
+++ bibleQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class bibleQueryOptions {
+public string book { get; set; }
+public int chapter { get; set; }
+public int verse { get; set; }
+}
+
+}
