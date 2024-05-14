1792ab057100db7dc8e009d30c75c73e2468628a
APIVerve Team <hello@apiverve.com>
2024-05-13 22:33:30 -0500

    Initial Commit

---
+++ QueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class QueryOptions {
+public string book { get; set; }
+public int chapter { get; set; }
+public int verse { get; set; }
+}
+
+}
